3e500ab519709bc14b0221328680da62f81b5616
paraswastaken <akarkaulia2000@gmail.com>
2022-06-02 22:16:40 +0530

    Improve some minor code and add click and drag to draw ability

--- script.js
+++ script.js
@@ -9,20 +9,32 @@ function createCanvas(n) {
         temp = document.createElement("div");
         temp.classList.add(`rows`);
         temp.style.height = `${512/n}px`;
+        temp.setAttribute('draggable', 'false');
         canv.appendChild(temp);
         for(j=0;j<n;j++){
             temp2 = document.createElement("div");
             temp2.classList.add(`pixels`);
+            temp2.setAttribute('draggable', 'false');
             temp.appendChild(temp2);
         }
     }  
 }
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 createCanvas(n);
 
+function addCol(e){
+    if(e.type === 'mouseover' && !mouseDown) return;
+
+    e.target.classList.add('color');
+}
+
 function evl(pix){
-    pix.addEventListener('mouseenter', (e)=>e.target.classList.add('color'));
-    pix.addEventListener('mouseleave', (e)=>e.target.classList.remove('color'));
+    pix.addEventListener('mousedown', addCol);
+    pix.addEventListener('mouseover', addCol);
 }
 
 const pixels = document.querySelectorAll('.pixels');
